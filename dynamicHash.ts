7cc8e16d2ebfc9838a5dd29142b47af05ea92d96
omerrj <omerrj99@gmail.com>
2024-03-09 02:27:50 +0300

    refactor: split hashes

--- dynamicHash.ts
+++ dynamicHash.ts
@@ -1,29 +1,3 @@
-function staticHash(key: number) {
-  return key % 3;
-}
-
-const buckets: Array<{ key: number; data: unknown }[]> = [[], [], []];
-
-function addStatic(key: number, data: unknown) {
-  const hashCode = staticHash(key);
-  buckets[hashCode].push({ key, data });
-}
-
-function searchStatic(key: number): unknown | null {
-  const hashCode = staticHash(key);
-  const bucket = buckets[hashCode];
-
-  for (const item of bucket) {
-    if (key === item.key) return item.data;
-  }
-
-  return null;
-}
-
-addStatic(123, "Paniry PUK");
-addStatic(432, "Paniry Kiri");
-addStatic(205, "Sharbati Rani");
-
 const dynamicHash = (key: number, n: number) => {
   return key % n;
 };
